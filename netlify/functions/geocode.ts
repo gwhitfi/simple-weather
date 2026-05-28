89ed488f1d134cc4e8e5c384503314eb004a7997
gwhitfi <justinwclemson@gmail.com>
2026-05-28 03:35:29 -0400

    fix bug with handler

--- netlify/functions/geocode.ts
+++ netlify/functions/geocode.ts
@@ -2,7 +2,7 @@ export const handler = async (event: any) => {
     const address = event.queryStringParameters.address;
     const url = `https://geocode.googleapis.com/v4/geocode/address/${address}?key=${process.env.GOOGLE_API_KEY}`;
     const response = await fetch(url);
-    const data = response.json();
+    const data = await response.json();
 
     return {
         statusCode: 200,
